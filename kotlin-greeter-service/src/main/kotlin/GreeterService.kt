e33478359cc8aee8b5f28244e280256f403d0392
Andrea Di Cesare <andrea@softinstigate.com>
2022-01-24 16:12:58 +0100

    :recycle: Refactor kotlinGreeterService
    g

--- kotlin-greeter-service/src/main/kotlin/GreeterService.kt
+++ kotlin-greeter-service/src/main/kotlin/GreeterService.kt
@@ -4,13 +4,13 @@ import org.restheart.exchange.ExchangeKeys.METHOD
 import org.restheart.plugins.JsonService
 import org.restheart.plugins.RegisterPlugin
 import org.restheart.utils.HttpStatus
-import org.restheart.utils.GsonUtils
+import org.restheart.utils.GsonUtils.`object` as obj
 
 @RegisterPlugin(name = "kotlinGreeterService", description = "just another Hello World in Kotlin")
 class GreeterService : JsonService {
     override fun handle(req: JsonRequest, res: JsonResponse) {
         when(req.method) {
-            METHOD.GET -> res.content = GsonUtils.`object`().put("msg", "Hello World").get()
+            METHOD.GET -> res.content = obj().put("msg", "Hello World").get()
             METHOD.OPTIONS -> handleOptions(req);
             else -> res.setStatusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
         }
